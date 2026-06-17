4cdd17377766b79bc826071e3043eea11ba5d3df
firetruck0312 <64617028+firetruck0312@users.noreply.github.com>
2026-06-17 14:50:06 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // Si publicarás en tu propio dominio, usa '/'. 
 // Si será en GitHub Pages (usuario.github.io/oxford-recibos), descomenta la línea:
-// const base = '/oxford-recibos/'
+ const base = '/oxford-recibos/'
 
 export default defineConfig({
   plugins: [react()],
